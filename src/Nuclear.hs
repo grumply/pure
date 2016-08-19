a243c70ffb1ceff14abbb1ed41579f754885a7a2
grumply <sean@grump.ly>
2016-08-19 05:41:47 -0500

    Switch to JSON-based messages.

--- src/Nuclear.hs
+++ src/Nuclear.hs
@@ -1,34 +1,54 @@
 {-# language DeriveGeneric #-}
+{-# language OverloadedStrings #-}
+{-# language CPP #-}
 module Nuclear
   ( Msg(..)
-  , Header
+  , Method
   , Body
   , fromBS
-  , module Data.Binary
+  , toBS
   , LazyByteString
   , Text
+  , module Data.Aeson
+  , module GHC.Generics
   ) where
 
-import Data.Binary
+import Data.Aeson
 import GHC.Generics
 import Data.Text
+import Data.Monoid
+import qualified Data.Text.Lazy as Lazy
+import qualified Data.Text.Lazy.Encoding as Lazy
 import qualified Data.ByteString.Lazy as LBS
 
 type LazyByteString = LBS.ByteString
 
-type Header = Text
-type Body = LazyByteString
+type Method = Text
+type Body = Lazy.Text
 
 data Msg
   = Msg
-    { msgHeader :: Header
-    , msgBody :: Body
+    { method :: Method
+    , body :: Body
     } deriving (Show,Eq,Ord,Generic)
 
-instance Binary Msg
+instance FromJSON Msg
+instance ToJSON Msg
+#if MIN_VERSION_aeson(0,10,0)
+  where
+    toEncoding = genericToEncoding defaultOptions
+#endif
 
 fromBS :: LazyByteString -> Either String Msg
-fromBS lbs =
-  case decodeOrFail lbs of
-    Left (_,_,msg) -> Left msg
-    Right (_,_,a) -> Right a
+fromBS = eitherDecode
+
+toBS :: Msg -> LazyByteString
+toBS = encode
+
+encodeMsg :: ToJSON a => Text -> a -> Msg
+encodeMsg method a =
+  let body = Lazy.decodeUtf8 $ encode a
+  in Msg {..}
+
+decodeMsg :: FromJSON a => Msg -> Maybe a
+decodeMsg Msg {..} = decode $ Lazy.encodeUtf8 body
