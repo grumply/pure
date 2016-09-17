b4dbf87e9c866f506c5dd6cc065961986170d840
grumply <sean@grump.ly>
2016-09-16 19:08:14 -0500

    Switch to String-based nuclear.

--- src/Nuclear.hs
+++ src/Nuclear.hs
@@ -9,79 +9,57 @@
 {-# language TypeFamilies #-}
 module Nuclear
   ( Msg(..), Name, Body
-  , fromBS, toBS
+  , fromS, toS
   , encodeMsg, decodeMsg
-  , LazyByteString, Text
-  , ToJSON(..), FromJSON(..)
   , Request(..), Response(..), Message(..)
-  , module GHC.Generics
   , module Data.Typeable
   , Proxy(..)
   ) where
 
-import Data.Aeson
-import GHC.Generics
-import Data.Text
 import Data.Monoid
-import qualified Data.Text.Lazy as Lazy
-import qualified Data.Text.Lazy.Encoding as Lazy
-import qualified Data.ByteString.Lazy as LBS
 
 import Data.Typeable
 import Data.Proxy
 
-type LazyByteString = LBS.ByteString
+import Text.Read
 
-type Name = Text
-type Body = Lazy.Text
+type Name = String
+type Body = String
 
 data Msg
   = Msg
     { name :: !Name
     , body :: !Body
-    } deriving (Show,Eq,Ord,Generic)
+    } deriving (Read,Show,Eq,Ord)
 
-instance FromJSON Msg
-instance ToJSON Msg
+fromS :: String -> Either String Msg
+fromS = readEither
 
+toS :: Msg -> String
+toS = show
 
--- keep an eye on this takeWhile; the other option is
--- conversion to strict ByteString with spanEnd. This
--- implementation could be problematic if sending a
--- bytestring of some pre-encoded value with a null
--- byte. If sticking to ToJSON/FromJSON this should
--- work out, I think.
-fromBS :: LazyByteString -> Either String Msg
-fromBS = eitherDecode . LBS.takeWhile (/= 0)
-
-toBS :: Msg -> LazyByteString
-toBS = encode
-
-encodeMsg :: ToJSON a => Text -> a -> Msg
+encodeMsg :: Show a => String -> a -> Msg
 encodeMsg name a =
-  let body = Lazy.decodeUtf8 $ encode a
+  let body = show a
   in Msg {..}
 
-decodeMsg :: FromJSON a => Msg -> Maybe a
-decodeMsg Msg {..} = decode $ Lazy.encodeUtf8 body
+decodeMsg :: Read a => Msg -> Maybe a
+decodeMsg Msg {..} = readMaybe body
 
-class (ToJSON a,FromJSON a,ToJSON b,FromJSON b,Typeable a,Typeable b) => Response a b | a -> b, b -> a where
-  requestName :: Proxy b -> Text
+class (Show a,Read a,Show b,Read b,Typeable a,Typeable b) => Response a b | a -> b, b -> a where
+  requestName :: Proxy b -> String
   requestName p =
-    append (pack "Req::")
-           (pack $ show $ typeRepTyCon $ typeOf (undefined :: b))
+     "Req::" ++ show (typeRepTyCon $ typeOf (undefined :: b))
 
-  responseName :: Proxy a -> Text
+  responseName :: Proxy a -> String
   responseName p =
-    append (pack "Res::")
-           (pack $ show $ typeRepTyCon $ typeOf (undefined :: a))
+    "Res::" ++ show (typeRepTyCon $ typeOf (undefined :: a))
 
-class (ToJSON a,FromJSON a,ToJSON b,FromJSON b,Typeable a,Typeable b) => Request b a | a -> b, b -> a
+class (Show a,Read a,Show b,Read b,Typeable a,Typeable b) => Request b a | a -> b, b -> a
 instance (Request b a) => Response a b
 
 -- class of unsollicited message types
-class (ToJSON m,FromJSON m,Typeable m) => Message m where
-  messageName :: Proxy m -> Text
+class (Show m,Read m,Typeable m) => Message m where
+  messageName :: Proxy m -> String
   messageName p =
-    append (pack "Msg::")
-           (pack $ show $ typeRepTyCon $ typeOf (undefined :: m))
+    "Msg::" ++ show (typeRepTyCon $ typeOf (undefined :: m))
