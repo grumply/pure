f70d335db599fd075d0a7943e91725af545e2417
sean <sean@grump.ly>
2017-01-01 13:24:04 -0600

    Add Try as a useful utility type for pending/completed/failed requests.

---
+++ src/Nuclear/Try.hs
@@ -0,0 +1,23 @@
+module Nuclear.Try where
+
+import Data.Aeson
+import GHC.Generics
+
+data Try a
+  = Trying
+  | Failed
+  | Done a
+  deriving (Eq,Generic,ToJSON,FromJSON)
+
+isTrying :: Try a -> Bool
+isTrying Trying = True
+isTrying _ = False
+
+isFailed :: Try a -> Bool
+isFailed Failed = True
+isFailed _ = False
+
+isDone :: Try a -> Bool
+isDone (Done _) = True
+isDone _ = False
+
