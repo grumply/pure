234186960e0048d2743c6c4fd21625a8e24ff9b3
S. M. Hickman <sean@grump.ly>
2017-01-21 14:36:11 -0600

    Add Lift instance to JSText.

--- src/Data/JSText.hs
+++ src/Data/JSText.hs
@@ -1,5 +1,7 @@
 {-# language CPP #-}
 #ifdef __GHCJS__
+{-# language TemplateHaskell #-}
+{-# language ViewPatterns #-}
 module Data.JSText (module Data.JSText, module Export) where
 import Data.JSString
 import Data.JSString.Text as Export
@@ -9,12 +11,19 @@ import JavaScript.JSON.Types.Instances as Export
 import JavaScript.JSON.Types as Export
 import JavaScript.JSON.Types.Internal as Export
 import JavaScript.JSON.Types.Generic as Export
+import Language.Haskell.TH.Syntax
+import GHCJS.Types (JSVal)
+import Data.Coerce
 
 type JSText = JSString
 
 -- this instance must be the same as Text to guarantee compatability
 instance Hashable JSText where
   hashWithSalt salt jss = hashWithSalt salt (textFromJSString jss)
+
+instance Lift JSString where
+  lift (unpack -> str) = [| pack str |]
+
 #else
 module Data.JSText (module Data.JSText, module Export) where
 import Data.Text
