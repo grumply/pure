33ece7658dbd741f4bd70f0ccd8313307424a40a
S. M. Hickman <sean@grump.ly>
2018-06-11 09:17:21 -0500

    Switch to a single module.

--- src/Pure.hs
+++ src/Pure.hs
@@ -1,4 +1,5 @@
-module HTML (module Export) where
+{-# LANGUAGE CPP #-}
+module Pure (module Export) where
 
 -- from pure-core
 import Pure.Data.View as Export hiding (On,content)
@@ -10,6 +11,15 @@ import Pure.Data.Default as Export
 -- from pure-dom
 import Pure.DOM as Export
 
+-- from pure-lifted
+import Pure.Data.Lifted as Export (Win(..),Doc(..),Head(..),Body(..),Element(..),Text(..),Node(..),Frag(..),IsNode(..),toJSV,Evt(..),Options(..),prevDef,prevProp,onRaw,(.#),findByTag,findById,same,isNull,getWindow,getBody,getDocument,getHead)
+import Pure.Animation as Export (addAnimation)
+import Pure.IdleWork as Export (addIdleWork)
+
+-- from pure-time
+import Pure.Data.Time as Export
+
+#ifdef __GHCJS__
 -- from pure-events
 import Pure.Data.Events as Export
 
@@ -17,14 +27,13 @@ import Pure.Data.Events as Export
 import Pure.Data.HTML as Export hiding (Head,Body,Style)
 import Pure.Data.HTML.Properties as Export hiding (Children,Data,Style,ContextMenu,Cite,Code,Form,Label,Span,Summary,Title,Bgcolor,Border,Color,Content,Height,Sizes,Width)
 
--- from pure-lifted
-import Pure.Data.Lifted as Export (Win(..),Doc(..),Head(..),Body(..),Element(..),Text(..),Node(..),Frag(..),IsNode(..),toJSV,Evt(..),Options(..),prevDef,prevProp,onRaw,(.#),findByTag,findById,same,isNull,getWindow,getBody,getDocument,getHead)
-import Pure.Animation as Export (addAnimation)
-import Pure.IdleWork as Export (addIdleWork)
-
 -- from pure-styles
 import Pure.Data.Styles as Export
 import Pure.Data.Styles.Patterns as Export
+#else
+-- from pure-server
+import Pure.Server as Export
 
--- from pure-time
-import Pure.Data.Time as Export
+-- from pure-websocket
+import Pure.WebSocket as Export hiding ((<||>))
+#endif
