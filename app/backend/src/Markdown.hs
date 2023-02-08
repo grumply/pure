df0153c67ca5feace6d727e72104d9730c0a2508
Sean Hickman <seanmhickman@gmail.com>
2023-02-07 23:58:41 -0600

    Simplify render with fromRight.

--- app/backend/src/Markdown.hs
+++ app/backend/src/Markdown.hs
@@ -3,6 +3,7 @@ module Markdown where
 import Shared (Markdown(..))
 
 import Pure (View)
+import Data.Either (fromRight)
 import Data.View.Parse (parseView)
 import Text.Pandoc.Class (runPure)
 import Text.Pandoc.Readers.Markdown (readMarkdown)
@@ -10,7 +11,7 @@ import Text.Pandoc.Writers.HTML (writeHtml5String)
 import Text.Pandoc.Options (def,ReaderOptions(..),pandocExtensions)
 
 render :: Markdown -> [View]
-render (Markdown md) = either (const []) id $ runPure $ do
+render (Markdown md) = fromRight [] $ runPure $ do
   one <- readMarkdown def { readerExtensions = pandocExtensions } md
   two <- writeHtml5String def one
   pure (parseView two)
