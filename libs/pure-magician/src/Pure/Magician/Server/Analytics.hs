205b50a730426257a45935d534ef6828d3f62432
Sean Hickman <seanmhickman@gmail.com>
2023-04-24 01:55:45 -0500

    Simplify imports.

--- libs/pure-magician/src/Pure/Magician/Server/Analytics.hs
+++ libs/pure-magician/src/Pure/Magician/Server/Analytics.hs
@@ -1,20 +1,14 @@
 module Pure.Magician.Server.Analytics where
 
 import Pure.Magician.Resources
-import Pure.Magician.Server.Serve
 
-import Pure.Conjurer hiding (cache)
 import qualified Pure.Conjurer as Conjurer
 import qualified Pure.Conjurer.Analytics as Conjurer
-import Pure.Convoker as Convoker
 import Data.JSON (ToJSON,FromJSON)
 import Data.Time (Time,pattern Day,delay)
 
-import Control.Concurrent
-import Control.Monad
-import Data.Typeable
-
-import Data.Hashable
+import Control.Concurrent (forkIO)
+import Control.Monad (void,forever)
 
 analyze :: forall a. (Server a, Subset (Analyze a) (Resources a) ~ True, Conjurer.Analyzeable (Analyze a)) => Time -> IO ()
 analyze d = do
