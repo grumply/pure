94c17ef408a270cf987975b88327f73389136162
Sean Hickman <seanmhickman@gmail.com>
2023-06-20 14:59:45 -0500

    Relocate Txt instances.

--- libs/pure/src/Data/Styles.hs
+++ libs/pure/src/Data/Styles.hs
@@ -2184,43 +2184,4 @@ alpha_ a = "alpha(" <> a <> ")"
 elems :: [Txt] -> Txt
 elems = Txt.intercalate ","
 
-instance IsString (Txt -> Txt) where
-  fromString s = (toTxt s <>)
-
-instance Num (Txt -> Txt) where
-  (-) a b = \x -> a x - b x
-  (+) a b = \x -> a x + b x
-  (*) a b = \x -> a x * b x
-  negate a = \x -> negate (a x)
-  abs = error "abs not defined for (Txt -> Txt)"
-  signum = error "signum not defined for (Txt -> Txt)"
-  fromInteger a = \x -> fromInteger a <> x
-
--- Note that css property names will have any spaces removed, 
--- so it is possible to use the `-` to construct property names:
--- 
--- > grid-template-columns => "grid - template - columns"
---   in CSS property names => "grid-template-columns"
-instance Num Txt where
-  (-) a b = a <> "-" <> b
-  (+) a b = a <> "+" <> b
-  (*) a b = a <> "*" <> b
-  negate x = " -" <> x
-  abs = error "abs not defined for Txt"
-  signum = error "signum not defined for Txt"
-  fromInteger i
-    | i < 0 = " " <> toTxt i
-    | otherwise = toTxt i
-
-instance Fractional Txt where
-  (/) a b = a <> "/" <> b
-  recip = error "recip not defined for Txt"
-  fromRational r
-    | r < 0 = " " <> toTxt ((fromRational :: Rational -> Double) r)
-    | otherwise = toTxt ((fromRational :: Rational -> Double) r)
-
-instance Fractional (Txt -> Txt) where
-  (/) a b = \x -> a x <> "/" <> b x
-  recip =  error "recip not defined for (Txt -> Txt)"
-  fromRational a = \x -> fromRational a <> x
 
--- libs/pure/src/Data/Txt.hs
+++ libs/pure/src/Data/Txt.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE CPP, PatternSynonyms, ViewPatterns, TypeSynonymInstances, OverloadedStrings, QuasiQuotes, BangPatterns #-}
+{-# LANGUAGE CPP, PatternSynonyms, ViewPatterns, TypeSynonymInstances, OverloadedStrings, QuasiQuotes, BangPatterns, FlexibleInstances #-}
 module Data.Txt 
   ( module Data.Txt 
   , module Export
@@ -26,6 +26,47 @@ import qualified Data.Text.Lazy.Encoding as TL
 
 import Data.Bits
 import Data.Word
+
+instance IsString (Txt -> Txt) where
+  fromString s = (toTxt s <>)
+
+instance Num (Txt -> Txt) where
+  (-) a b = \x -> a x - b x
+  (+) a b = \x -> a x + b x
+  (*) a b = \x -> a x * b x
+  negate a = \x -> negate (a x)
+  abs = error "abs not defined for (Txt -> Txt)"
+  signum = error "signum not defined for (Txt -> Txt)"
+  fromInteger a = \x -> fromInteger a <> x
+
+-- Note that css property names will have any spaces removed, 
+-- so it is possible to use the `-` to construct property names:
+-- 
+-- > grid-template-columns => "grid - template - columns"
+--   in CSS property names => "grid-template-columns"
+instance Num Txt where
+  (-) a b = a <> "-" <> b
+  (+) a b = a <> "+" <> b
+  (*) a b = a <> "*" <> b
+  negate x = " -" <> x
+  abs = error "abs not defined for Txt"
+  signum = error "signum not defined for Txt"
+  fromInteger i
+    | i < 0 = " " <> toTxt i
+    | otherwise = toTxt i
+
+instance Fractional Txt where
+  (/) a b = a <> "/" <> b
+  recip = error "recip not defined for Txt"
+  fromRational r
+    | r < 0 = " " <> toTxt ((fromRational :: Rational -> Double) r)
+    | otherwise = toTxt ((fromRational :: Rational -> Double) r)
+
+instance Fractional (Txt -> Txt) where
+  (/) a b = \x -> a x <> "/" <> b x
+  recip =  error "recip not defined for (Txt -> Txt)"
+  fromRational a = \x -> fromRational a <> x
+
 
 {-
 It is important to note that ToTxt/FromTxt can throw exceptions!
