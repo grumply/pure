995288a884a65e7e45efe1063c4d1d5b8aeda154
Sean Hickman <seanmhickman@gmail.com>
2023-12-09 16:11:56 -0600

    Add a polykind nominal phantom type variable to Slug.

--- libs/pure/src/Data/Slug.hs
+++ libs/pure/src/Data/Slug.hs
@@ -1,4 +1,4 @@
-{-# language DerivingVia, CPP #-}
+{-# language DerivingVia, CPP, RoleAnnotations, PolyKinds #-}
 module Data.Slug (Slug()) where
 
 import Data.Char
@@ -8,48 +8,46 @@ import Data.Router
 import Data.String
 import Data.Txt as Txt
 
-newtype Slug = Slug Txt
+newtype Slug (x :: k) = Slug Txt
   deriving 
     (Eq,Ord,ToJSON,FromJSON,Hashable
 #ifndef __GHCJS__
     , ToJSONKey, FromJSONKey
 #endif
     ) via Txt
+type role Slug nominal
 
-instance ToTxt Slug where
-  {-# INLINE toTxt #-}
+instance ToTxt (Slug a) where
   toTxt (Slug x) = x
 
-instance FromTxt Slug where
-  {-# INLINE fromTxt #-}
+instance FromTxt (Slug a) where
   fromTxt = toSlug
 
-instance IsString Slug where
-  {-# INLINE fromString #-}
+instance IsString (Slug a) where
   fromString = toSlug . fromTxt . toTxt
 
 -- Idempotent.
 --
 -- prop> \(x :: String) -> toSlug (toTxt (toSlug (toTxt x))) == toSlug (toTxt x)
 -- 
-toSlug :: Txt -> Slug
+toSlug :: Txt -> Slug a
 toSlug = Slug . Txt.dropWhileEnd (Prelude.not . isAlphaNum) . Txt.dropWhile (Prelude.not . isAlphaNum) . process . Txt.take 255
-    where 
-      safe c = c == '.' || isAscii c && isAlphaNum c
+  where 
+    safe c = c == '.' || isAscii c && isAlphaNum c
 
-      process :: Txt -> Txt
-      process = go False
-        where
-          go dropping x =
-            case Txt.uncons x of
-              Just (c,rest) -> 
-                let 
-                  d = isAscii c && Prelude.not (isAlphaNum c) 
-                  l | dropping && d = id
-                    | safe c        = Txt.cons (Data.Char.toLower c)
-                    | otherwise     = Txt.cons '-'
-                in
-                  l (go d rest)
+    process :: Txt -> Txt
+    process = go False
+      where
+        go dropping x =
+          case Txt.uncons x of
+            Just (c,rest) -> 
+              let 
+                d = isAscii c && Prelude.not (isAlphaNum c) 
+                l | dropping && d = id
+                  | safe c        = Txt.cons (Data.Char.toLower c)
+                  | otherwise     = Txt.cons '-'
+              in
+                l (go d rest)
 
-              _ -> x
-{-# INLINE toSlug #-}
+            _ -> x
+{-# NOINLINE toSlug #-}
